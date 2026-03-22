b08bf10d6079e2b57ba1fb0ad06def9e70680381
Артём Костылев <ddddddd422@gmail.com>
2026-03-22 17:51:55 +0700

    Реализована заготовка MigrationService

--- MigrationService/Program.cs
+++ MigrationService/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using UserService.Infrastructure;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        var connectionString = context.Configuration.GetConnectionString("Default");
+
+        services.AddDbContext<UserDbContext>(options =>
+            options.UseNpgsql(connectionString));
+    })
+    .Build();
+
+using var scope = host.Services.CreateScope();
+
+var services = scope.ServiceProvider;
+
+try
+{
+    Console.WriteLine("Применяются миграции...");
+
+    var userDb = services.GetRequiredService<UserDbContext>();
+
+    await userDb.Database.MigrateAsync();
+
+    Console.WriteLine("Миграции успешно применены...");
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Ошибка при применении миграции: {0}", ex);
+    throw;
+}
