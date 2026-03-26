3c8b96d51539a4afbddaefcf0c329b9f8a36a131
kostylev-as <kostylev-as@cityair.ru>
2026-03-26 21:51:43 +0700

    Добавлены unit тесты для контроллеров FinanceService и UserService

---
+++ UserService.Tests/UserControllerTests.cs
@@ -0,0 +1,82 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using FluentAssertions;
+using MediatR;
+using Moq;
+using UserService.API;
+using UserService.Application.Common;
+using UserService.Application.User.Commands;
+
+namespace UserService.Tests
+{
+    public class UserControllerTests
+    {
+        private readonly Mock<IMediator> mediatorMock;
+        private readonly UserController controller;
+
+        public UserControllerTests()
+        {
+            mediatorMock = new Mock<IMediator>();
+            controller = new UserController(mediatorMock.Object);
+        }
+
+        [Fact]
+        public async Task Register_Should_Call_Mediator()
+        {
+            var command = new RegisterUserCommand("user", "password");
+
+            mediatorMock.Setup(x => x.Send(command, default)).Returns(Task.CompletedTask);
+
+            var result = await controller.Register(command);
+
+            result.Should().BeOfType<OkResult>();
+
+            mediatorMock.Verify(x => x.Send(command, default),Times.Once);
+        }
+
+        [Fact]
+        public async Task Login_Should_Return_Ok_When_Success()
+        {
+            var command = new LoginUserCommand("name", "password");
+
+            var response = new Result { Success = true, Message = "token" };
+
+            mediatorMock.Setup(x => x.Send(command, default)).ReturnsAsync(response);
+
+            var result = await controller.Login(command);
+
+            result.Should().BeOfType<OkObjectResult>();
+        }
+
+        [Fact]
+        public async Task Login_Should_Return_Unauthorized_When_Failed()
+        {
+            var command = new LoginUserCommand("user", "password");
+
+            var response = new Result { Success = false, Error = "Invalid credentials" };
+
+            mediatorMock.Setup(x => x.Send(command, default)).ReturnsAsync(response);
+
+            var result = await controller.Login(command);
+
+            result.Should().BeOfType<UnauthorizedObjectResult>();
+        }
+
+        [Fact]
+        public async Task TestAuthorize_Should_Return_Ok_When_User_Exists()
+        {
+            var claims = new[] { new Claim(ClaimTypes.NameIdentifier, "user-id") };
+
+            var identity = new ClaimsIdentity(claims, "TestAuth");
+
+            var user = new ClaimsPrincipal(identity);
+
+            controller.ControllerContext = new ControllerContext { HttpContext = new DefaultHttpContext { User = user } };
+
+            var result = await controller.TestAuthorize();
+
+            result.Should().BeOfType<OkObjectResult>();
+        }
+    }
+}
